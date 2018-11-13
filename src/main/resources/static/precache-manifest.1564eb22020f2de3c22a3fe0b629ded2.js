a736999cfb5164cf93430bc987712d1b22a3c514
Buzwe Mfaca <bmfaca@gmail.com>
2018-11-13 23:57:22 +0200

    reactjs

---
+++ src/main/resources/static/precache-manifest.1564eb22020f2de3c22a3fe0b629ded2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4e495b04d444a63512e9",
+    "url": "/static/js/main.4e495b04.chunk.js"
+  },
+  {
+    "revision": "3ee9bcce5228eddabbd5",
+    "url": "/static/js/1.3ee9bcce.chunk.js"
+  },
+  {
+    "revision": "4e495b04d444a63512e9",
+    "url": "/static/css/main.4add3bdf.chunk.css"
+  },
+  {
+    "revision": "3ee9bcce5228eddabbd5",
+    "url": "/static/css/1.4e63c946.chunk.css"
+  },
+  {
+    "revision": "6a90f200dbb2006fd4ba654f9e417c1b",
+    "url": "/index.html"
+  }
+];
